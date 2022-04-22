eb71fe7a357b11cff3429b65d4d9088e6672f951
Josefin Olsen <56364674+JoshisIsland@users.noreply.github.com>
2022-04-22 11:43:29 +0200

    methods in edge

--- src/Edge.java
+++ src/Edge.java
@@ -1,2 +1,22 @@
 public class Edge {
+    public void getDestination() {
+
+    }
+
+    public void getWeight() {
+
+    }
+
+    public void setWeight() {
+
+    }
+
+    public void getName() {
+
+    }
+
+    public String toString() {
+        return "test";
+    }
+
 }
