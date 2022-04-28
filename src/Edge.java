c4bd90ac67d9f9bf1744564058621d11ba5499d8
Josefin Olsen <56364674+JoshisIsland@users.noreply.github.com>
2022-04-28 12:16:02 +0200

    Edge konstruktor

--- src/Edge.java
+++ src/Edge.java
@@ -5,6 +5,16 @@ public class Edge {
     private String name;
     private double weight;
 
+    public Edge(Town destination, String name, double weight) {
+        this.destination = Objects.requireNonNull(destination);
+        this.name = Objects.requireNonNull(name);
+
+        if (Double.isNaN(weight)) {
+            throw new IllegalArgumentException();
+        }
+        this.weight = weight;
+    }
+
     public Object getDestination() {
         return destination;
     }
