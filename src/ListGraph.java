df9dcb9291866c7f0da5ef41f80b19bcc378f005
BarbaFin <fogelberg-rydberg@hotmail.com>
2022-05-03 14:49:37 +0200

    ui

--- src/ListGraph.java
+++ src/ListGraph.java
@@ -31,10 +31,10 @@ public class ListGraph {
         Set<Edge> xTownEdge = nodes.get(x);
         Set<Edge> yTownEdge = nodes.get(y);
 
-        yTownEdge.remove(nodes.get(x));
-        xTownEdge.remove(nodes.get(y));
+        //yTownEdge.remove(nodes.get(x));
+        //xTownEdge.remove(nodes.get(y));
 
-         System.out.println(": " + xTownEdge);
+        System.out.println(": " + xTownEdge);
         System.out.println(": " + yTownEdge);
 
     }
