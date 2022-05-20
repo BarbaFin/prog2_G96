586ceaa3452c0c835bbac887f5f03c80fb1ccd72
BarbaFin <fogelberg-rydberg@hotmail.com>
2022-05-20 16:13:06 +0200

    Update ShowConnectionDialog.java

--- src/ShowConnectionDialog.java
+++ src/ShowConnectionDialog.java
@@ -8,22 +8,25 @@ import javafx.scene.layout.GridPane;
 public class ShowConnectionDialog extends Alert {
     private static TextField name = new TextField();
     private static TextField time = new TextField();
-    private String ja;
+    private String edgeName;
 
-    private int nej;
+    private int edgeTime;
 
-    public ShowConnectionDialog(String c1, String c2, String ja, int nej){
+    public ShowConnectionDialog(String c1, String c2, String edgeName, int edgeTime){
         super(AlertType.CONFIRMATION);
-        this.ja = ja;
-        this.nej = nej;
+        this.edgeName = edgeName;
+        this.edgeTime = edgeTime;
         GridPane grid = new GridPane();
         grid.setPadding(new Insets(10));
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(5);
         grid.setVgap(5);
 
-        grid.addRow(0, new Label(ja), name);
-        String s = String.valueOf(nej);
+        //DET SKA STÅ NAME: (STRING I TEXTFIELD, EJ RÖRBART)
+        //DET SKA STÅ TIME: (TIME I TEXTFIELD, EJ RÖRBART)
+
+        grid.addRow(0, new Label(edgeName), name);
+        String s = String.valueOf(edgeTime);
         grid.addRow(1, new Label(s), time);
 
         getDialogPane().setContent(grid);
